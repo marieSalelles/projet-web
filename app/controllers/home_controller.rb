c6cb2e7ed87adc1939e0f9f5df7c9a620e0c4c7f
marieSalelles <mariesalelles@gmail.com>
2018-05-31 17:02:09 +0200

    reglage probleme desc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @objets= Objet.where("datefinench > ?" , DateTime.now).order(:created_at).limit(6)
+    @objets= Objet.where("datefinench > ?" , DateTime.now).order(created_at: :desc).limit(6)
     @categories= Categorie.all
 
   end
